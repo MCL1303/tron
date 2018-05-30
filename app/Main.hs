5ea96a94933378d24eb9d89dc80dc09bff52e041
Timofey <36996576+De3up@users.noreply.github.com>
2018-05-30 18:08:50 +0300

    Add black game board

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main where
 
-import Lib
+
+import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
-main = someFunc
+main =
+    play display bgColor fps initialWorld renderWorld handleWorld updateWorld
+  where
+    display = InWindow "Game of Trons" winSize winOffset
+    bgColor = black
+    fps = 60
+    initialWorld = ()
+    renderWorld _ = blank
+    handleWorld _ w = w
+    updateWorld _ w = w
+    winSize = ( 800, 800 )
+    winOffset = ( 100, 100 )
